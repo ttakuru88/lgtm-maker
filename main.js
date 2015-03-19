9c9f7254f8fed2c63aae8ac769dc0a22c3e94320
Takuru <takuru@hai3.net>
2015-03-19 18:19:12 +0900

    ドラッグアンドドロップで画像を選択できる

--- main.js
+++ main.js
@@ -13,54 +13,15 @@ $(function(){
   var $bold = $('#bold');
   var $italic = $('#italic');
 
+  $(document).on('dragover', function(e){
+    e.preventDefault()
+  }).on('drop', function(e){
+    e.preventDefault()
+    setImage(e.originalEvent.dataTransfer.files[0]);
+  });
+
   $file.on('change', function(e){
-    if(!e.target.files[0]) { return; }
-
-    var imageReader = new FileReader;
-
-    imageReader.onload = function(e){
-      var image = new Image;
-
-      image.onload = function(){
-        var fabricImage = new fabric.Image(image);
-        var aspect = fabricImage.width / fabricImage.height;
-        var width = Math.min(image.width, maxWidth)
-
-        fabricImage.set({
-          selectable: false,
-          width:  width,
-          height: width / aspect
-        });
-
-        canvas.setWidth(fabricImage.width);
-        canvas.setHeight(fabricImage.height);
-
-        lgtmText = new fabric.Text($text.val());
-        lgtmText.set({
-          fontSize: 64,
-          fontFamily: $fontFamily.val(),
-          stroke: '#000',
-          strokeWidth: 2,
-          fill: '#fff',
-          left: (fabricImage.width - lgtmText.width) / 2,
-          top:  (fabricImage.height - lgtmText.height) / 2,
-          cornerSize: 6,
-          cornerColor: '#6699ff',
-          transparentCorners: false,
-          fontWeight: ($bold.is(':checked') ? 'bold' : null),
-          fontStyle: ($italic.is(':checked') ? 'italic' : null)
-        });
-
-        canvas.clear()
-        canvas.add(fabricImage);
-        canvas.add(lgtmText);
-      };
-      image.src = e.target.result;
-    };
-
-    imageReader.readAsDataURL(e.target.files[0]);
-
-    $download.attr('download', 'LGTM-' + e.target.files[0].name);
+    setImage(e.target.files[0]);
   });
 
   $download.on('click', function(){
@@ -117,4 +78,55 @@ $(function(){
     lgtmText.setFontStyle($italic.is(':checked') ? 'italic' : null);
     canvas.renderAll();
   });
+
+
+  var setImage = function(file){
+    if(!file) { return; }
+
+    var imageReader = new FileReader;
+
+    imageReader.onload = function(e){
+      var image = new Image;
+
+      image.onload = function(){
+        var fabricImage = new fabric.Image(image);
+        var aspect = fabricImage.width / fabricImage.height;
+        var width = Math.min(image.width, maxWidth)
+
+        fabricImage.set({
+          selectable: false,
+          width:  width,
+          height: width / aspect
+        });
+
+        canvas.setWidth(fabricImage.width);
+        canvas.setHeight(fabricImage.height);
+
+        lgtmText = new fabric.Text($text.val());
+        lgtmText.set({
+          fontSize: 64,
+          fontFamily: $fontFamily.val(),
+          stroke: '#000',
+          strokeWidth: 2,
+          fill: '#fff',
+          left: (fabricImage.width - lgtmText.width) / 2,
+          top:  (fabricImage.height - lgtmText.height) / 2,
+          cornerSize: 6,
+          cornerColor: '#6699ff',
+          transparentCorners: false,
+          fontWeight: ($bold.is(':checked') ? 'bold' : null),
+          fontStyle: ($italic.is(':checked') ? 'italic' : null)
+        });
+
+        canvas.clear()
+        canvas.add(fabricImage);
+        canvas.add(lgtmText);
+      };
+      image.src = e.target.result;
+    };
+
+    imageReader.readAsDataURL(file);
+
+    $download.attr('download', 'LGTM-' + file.name);
+  };
 });
